f07674e660b25d749f11dce025524639961c132f
Vincent Franstyo <sorakiriko28814@gmail.com>
2023-04-19 12:04:45 +0700

    add : added time adding but with error

--- Time.java
+++ Time.java
@@ -11,6 +11,48 @@ public class Time {
         this.second = second;
     }
 
+    public Time(int hour, int minute, int second) {
+        this(0, hour, minute, second);
+    }
+
+    public Time(int minute, int second) {
+        this(0, 0, minute, second);
+    }
+
+    public Time(int second) {
+        this(0, 0, 0, second);
+    }
+
+    public Time() {
+        this(0, 0, 0, 0);
+    }
+
+    public Time(int day1, int hour1, int minute1, int second1, int day2, int hour2, int minute2, int second2) {
+        this(day1, hour1, minute1, second1);
+        this.day += day2;
+        this.hour += hour2;
+        this.minute += minute2;
+        this.second += second2;
+    }
+
+    public Time(int hour1, int minute1, int second1, int hour2, int minute2, int second2) {
+        this(hour1, minute1, second1);
+        this.hour += hour2;
+        this.minute += minute2;
+        this.second += second2;
+    }
+
+    public Time(int minute1, int second1, int minute2, int second2) {
+        this(minute1, second1);
+        this.minute += minute2;
+        this.second += second2;
+    }
+
+    public Time(int second1, int second2) {
+        this(second1);
+        this.second += second2;
+    }
+
     public int getDay() {
         return day;
     }
