811fc9e27fd40b2dfd7b1550429c6f080e457921
Rei Arriel Clyfton <92192967+RClyf@users.noreply.github.com>
2023-04-21 23:34:24 +0700

    World

---
+++ app/src/main/java/com/BNMO/World.java
@@ -0,0 +1,21 @@
+package com.BNMO;
+
+public class World {
+    private int length;
+    private int width;
+
+    public World(){
+        this.length = 64;
+        this.width = 64;
+    }
+
+    public int getLength(){
+        return length;
+    }
+
+    public int getWidth(){
+        return width;
+    }
+
+
+}
