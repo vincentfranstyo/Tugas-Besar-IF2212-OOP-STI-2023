b224a0fe31415d4b375854ea9cf0b70f8de66047
Rei Arriel Clyfton <92192967+RClyf@users.noreply.github.com>
2023-04-22 14:48:03 +0700

    Update World

--- app/src/main/java/com/BNMO/World.java
+++ app/src/main/java/com/BNMO/World.java
@@ -1,12 +1,22 @@
 package com.BNMO;
 
+import com.BNMO.Buildings.House;
+import com.BNMO.Utilities.Point;
+
+import java.util.ArrayList;
+
 public class World {
     private int length;
     private int width;
+    private ArrayList<House> houseList;
+    private int totalBuild;
 
     public World(){
         this.length = 64;
         this.width = 64;
+        this.houseList = new ArrayList<>();
+        totalBuild = 0;
+
     }
 
     public int getLength(){
@@ -15,6 +25,22 @@ public class World {
 
     public int getWidth(){
         return width;
+    }
+
+    public ArrayList<House> getObjects() {
+        return houseList;
+    }
+
+    public void addHouse(Point point){
+
+    }
+
+    public void removeHouse(House house){
+
+    }
+
+    public int getTotalBuild(){
+        return totalBuild;
     }
 
 
