5bbce6e470e03c27d27a65a77cd0a27784dab9ed
Rei Arriel Clyfton <92192967+RClyf@users.noreply.github.com>
2023-04-24 17:18:57 +0700

    Main Menu

---
+++ app/src/main/java/com/BNMO/Main.java
@@ -0,0 +1,31 @@
+package com.BNMO;
+
+import com.BNMO.SIMS.Sim;
+
+public class Main {
+    private boolean gameStarted = false;
+
+    public void start(){
+        this.gameStarted = true;
+    }
+
+    public void help(){
+        System.out.println("blablabla");
+    }
+
+    public void exit(){
+        this.gameStarted = false;
+    }
+
+    public void viewSimInfo(Sim sim){
+        System.out.println("Info Sim");
+    }
+
+    public void addSim(){
+        
+    }
+
+    public void changeSim(){
+        
+    }
+}
