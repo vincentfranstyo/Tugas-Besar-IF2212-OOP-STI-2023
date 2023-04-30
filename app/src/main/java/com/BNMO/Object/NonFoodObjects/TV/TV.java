5aa9957ec38a948118f060a265dba99308b9c34e
rayhanmp <90276351+rayhanmp@users.noreply.github.com>
2023-04-30 20:58:43 +0700

    Penambahan menu untuk TV dan perbaikan berbagai method

--- app/src/main/java/com/BNMO/Object/NonFoodObjects/TV/TV.java
+++ app/src/main/java/com/BNMO/Object/NonFoodObjects/TV/TV.java
@@ -6,18 +6,16 @@ import com.BNMO.Utilities.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class TV extends NonFoodObjects {
-    // this class also has a list of channel
     private List<Channel> channels;
-    private Channel currentChannel;
     private boolean isOn;
     private boolean hasScanned;
 
-    public TV(String name, Channel currentChannel) {
+    public TV(String name) {
         super(name, 2, 2, 200);
         this.channels = new ArrayList<>();
-        this.currentChannel = currentChannel;
         this.isOn = false;
         this.hasScanned = false;
         this.setType("TV");
@@ -27,12 +25,28 @@ public class TV extends NonFoodObjects {
         return isOn;
     }
 
+    public void setIsOn(boolean isOn) {
+        this.isOn = isOn;
+    }
+
     public void turnOn() {
-        this.isOn = true;
+        if (getIsOn()) {
+            System.out.println("The TV is already on");
+        }
+        else {
+            setIsOn(true);
+            System.out.println("The TV is on");
+        }
     }
 
     public void turnOff() {
-        this.isOn = false;
+        if (getIsOn()) {
+            setIsOn(false);
+            System.out.println("The TV is off");
+        }
+        else {
+            System.out.println("The TV is already off");
+        }
     }
 
     public boolean getHasScanned() {
@@ -43,22 +57,13 @@ public class TV extends NonFoodObjects {
         this.hasScanned = hasScanned;
     }
 
-    public Channel getCurrentChannel() {
-        if (getIsOn()) {
-            return currentChannel;
-        } else {
-            System.out.println("The TV is off");
-            return null;
-        }
-    }
-
     public void scanChannel() {
         if (getIsOn()) {
-            if (getHasScanned() == false) {
+            if (!getHasScanned()) {
                 System.out.println("Scanning channel...");
                 setHasScanned(true);
                 channels.add(new Channel("Republican TV", 1, "Politics"));
-                channels.add(new Channel("Democrat TV", 2, "Politics"));
+                channels.add(new Channel("Democrat T        V", 2, "Politics"));
                 channels.add(new Channel("Historian TV", 3, "Education"));
                 channels.add(new Channel("HBU Max", 4, "Movies"));
                 channels.add(new Channel("HBU GO", 5, "Movies"));
@@ -146,44 +151,52 @@ public class TV extends NonFoodObjects {
         }
     }
 
-    public void setCurrentChannel(String channelName) {
-        if (getIsOn()) {
-            if (!getHasScanned()) {
-                System.out.println("You haven't scanned for channels yet");
-            } else {
-                boolean foundChannel = false;
-                for (Channel channel : channels) {
-                    if (channel != null && channel.getName().equals(channelName)) {
-                        foundChannel = true;
-                        setCurrentChannel(channel.getName());
-                        break;
-                    }
-                }
-                if (!foundChannel) {
-                    System.out.println("Channel not found");
-                }
-            }
-        } else {
-            System.out.println("The TV is off");
-        }
-    }
-
-    public void watchTV(Time time, Sim sim) {
+    public void watchTV(Time time, Sim sim, Channel channel) {
         try {
             if (!getIsOccupied()) {
                 setIsOccupied(true);
                 if (getIsOn()) {
                     if (sim.getStatus().equals("Nothing")) {
-                        int duration = time.convertToSecond();
-                        System.out.println(sim.getName() + "is watching TV for " + duration + " seconds");
-                        Thread.sleep(duration * 1000);
-                        sim.setStatus("Watching TV");
-                        sim.setMood(sim.getMood() + (4 * duration / 20));
-                        sim.setHealth(sim.getHealth() - (3 * duration / 20));
-                        sim.setFullness(sim.getFullness() - (2 * duration / 20));
-                    } else {
-                        System.out.println("You can't watch TV while you are " + sim.getStatus());
+                        if (getHasScanned()) {
+                            int duration = time.convertToSecond();
+                            System.out.println(sim.getName() + "is watching TV for " + duration + " seconds");
+                            Thread.sleep(duration * 1000);
+                            sim.setStatus("Watching TV");
+                            if (channel.getGenre().equals("Sports")) {
+                                int moodChange = (int) (Math.random() * 7) - 3;
+                                sim.setMood(sim.getMood() + (3 * duration / 20) + moodChange);
+                                sim.setHealth(sim.getHealth() - (2 * duration / 20));
+                                sim.setFullness(sim.getFullness() - (2 * duration / 20));
+                            } else if (channel.getGenre().equals("Movies")) {
+                                sim.setMood(sim.getMood() + (3 * duration / 20));
+                                sim.setHealth(sim.getHealth() - (3 * duration / 20));
+                                sim.setFullness(sim.getFullness() - (2 * duration / 20));
+                            } else if (channel.getGenre().equals("Comedy")) {
+                                sim.setMood(sim.getMood() + (6 * duration / 20));
+                                sim.setHealth(sim.getHealth() - (2 * duration / 20));
+                                sim.setFullness(sim.getFullness() - (2 * duration / 20));
+                            } else if (channel.getGenre().equals("Politics")) {
+                                sim.setMood(sim.getMood() - (1 * duration / 20));
+                                sim.setHealth(sim.getHealth() - (2 * duration / 20));
+                                sim.setFullness(sim.getFullness() - (2 * duration / 20));
+                            } else if (channel.getGenre().equals("Education")) {
+                                sim.setMood(sim.getMood() + (2 * duration / 20));
+                                sim.setHealth(sim.getHealth() - (2 * duration / 20));
+                                sim.setFullness(sim.getFullness() - (2 * duration / 20));
+                            }
+                            else {
+                                sim.setMood(sim.getMood() + (2 * duration / 20));
+                                sim.setHealth(sim.getHealth() - (3 * duration / 20));
+                                sim.setFullness(sim.getFullness() - (2 * duration / 20));
+                            }
+                            }
+                        else {
+                            System.out.println("You haven't scanned for channels yet");
+                        }
                     }
+                else {
+                    System.out.println("You can't watch TV while you are " + sim.getStatus());
+                }
                 } else {
                     System.out.println("The TV is off");
                 }
@@ -197,4 +210,85 @@ public class TV extends NonFoodObjects {
         }
     }
 
+public void menuTV(Sim sim) {
+    Scanner scanner = new Scanner(System.in);
+    boolean exit = false;
+
+    System.out.println("Hello " + sim.getName() + "!");
+    while (!exit) {
+        System.out.println("What would you like to do?");
+        System.out.println("[1] Turn on the TV");
+        System.out.println("[2] Turn off the TV");
+        System.out.println("[3] Scan for channels");
+        System.out.println("[4] Add a channel");
+        System.out.println("[5] Remove a channel");
+        System.out.println("[6] Show available channels");
+        System.out.println("[7] Watch TV");
+        System.out.println("[8] Go back to main menu");
+        System.out.print("Enter your choice: ");
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                turnOn();
+                break;
+            case 2:
+                turnOff();
+                System.out.print("The TV is off");
+                break;
+            case 3:
+                scanChannel();
+                System.out.print("Successfully added 10 channels!");
+                break;
+            case 4:
+                System.out.print("Enter channel name: ");
+                String name = scanner.next();
+                System.out.print("Enter channel genre: ");
+                String genre = scanner.next();
+                addChannel(name, genre);
+                break;
+            case 5:
+                System.out.print("Enter channel ID: ");
+                try {
+                    int id = scanner.nextInt();
+                    removeChannel(id);
+                } catch (InputMismatchException e) {
+                    System.out.println("Invalid input. Please enter a valid integer for the channel ID.");
+                    scanner.nextLine(); // Consume the invalid input to avoid an infinite loop
+                }
+                break;
+            case 6:
+                showChannel();
+                break;
+            case 7:
+                System.out.print("How long do you want to watch the TV?");
+                System.out.print("Enter the duration in seconds: ");
+                int duration = scanner.nextInt();
+                System.out.println("Which channel do you want to watch?");
+                showChannel();
+                System.out.print("Enter channel name: ");
+                scanner.nextLine(); // Consume the newline character left by scanner.nextInt()
+                String channelName = scanner.nextLine();
+
+                boolean foundChannel = false;
+                    for(Channel channel: channels) {
+                        if(channel.getName().equalsIgnoreCase(channelName)) {
+                            watchTV(new Time(duration), sim, channel);
+                            foundChannel = true;
+                            break;
+                        }
+                    }
+                    if(!foundChannel) {
+                        System.out.println("Invalid channel name or the channel doesn't exist. Please try again.");
+                    }
+                    break;
+            case 8:
+                exit = true;
+                break;
+            default:
+                System.out.println("Invalid choice. Please try again.");
+                break;
+        }
+    }
+}
 }
